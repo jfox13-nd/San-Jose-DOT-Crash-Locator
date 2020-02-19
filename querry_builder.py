7bad29e3b8c00b31e988bddf17aa15904093855a
Jack Fox <jfox13@nd.edu>
2020-02-19 15:22:20 -0800

    removed redundant replace

--- querry_builder.py
+++ querry_builder.py
@@ -13,7 +13,7 @@ def replace_string_builder(replace_json: str, col_name: str) -> str:
     ''' builds the piece of a querry responsible for doing all string replacements to clean data '''
     with open(replace_json, 'r') as f:
         replace_dict = json.load(f)
-    replace_string = "replace({}, \'\', \'\')".format(col_name)
+    replace_string = col_name
 
     for key in replace_dict:
         replace_string = "replace({}, {}, {})".format(replace_string, key, replace_dict[key])
