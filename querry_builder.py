14512bda558bc8dd432797e610ab2f3e3efbef21
Jack Fox <jfox13@nd.edu>
2020-02-21 10:24:06 -0800

    made querry_builder regex workable, began experimenting with db connections

--- querry_builder.py
+++ querry_builder.py
@@ -5,9 +5,13 @@ __author__ = "Jack Fox"
 __email__ = "jfox13@nd.edu"
 
 import json
+import psycopg2
+import sys
 
-STRINGCONVERSIONJSON = "conversion.json"
+STRINGCONVERSIONJSON = "conversions.json"
 DBNAME = "?????"
+DBLOCALNAME = "dot"
+USERNAME = "jfox13"
 
 def replace_string_builder(replace_json: str, col_name: str) -> str:
     ''' builds the piece of a querry responsible for doing all string replacements to clean data '''
@@ -16,12 +20,25 @@ def replace_string_builder(replace_json: str, col_name: str) -> str:
     replace_string = col_name
 
     '''
-    TODO: fix replace logic, should only find and replace when for the following cases:
-    * " substring "
-    * "^substring "
-    * " substring"
+    Error in regex: REGEXP_REPLACE() does not allow for capture groups so string replacements might create odd formatting by adding excess spaces
+    ex. "^Example ct.$" -> "^Example court $"
     '''
     for key in replace_dict:
-        replace_string = "replace({}, \'{}\', \'{}\')".format(replace_string, key, replace_dict[key])
+        replace_string = "REGEXP_REPLACE({}, \'(^| ){}($| )\', \'{}\','g')".format(replace_string, key, replace_dict[key])
 
     return replace_string
+
+if __name__ == '__main__':
+    try:
+        connection = psycopg2.connect(user = USERNAME,
+                                    host = "127.0.0.1",
+                                    port = "5432",
+                                    database = DBLOCALNAME)
+        cursor = connection.cursor()
+    except:
+        print("Error: Could not connect to SQL database {} as {}".format(DBLOCALNAME,USERNAME),file=sys.stderr)
+    #print('select {}, {} from "Intersections" limit 2'.format(replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".astreetnam)'),replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".astreetnam)')))
+    #cursor.execute('select {}, {} from "Intersections" limit 2'.format(replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".astreetnam)'),replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".bstreetnam)')))
+    cursor.execute('select {}, {} from "Intersections" limit 2'.format('LOWER("Intersections".astreetnam)','LOWER("Intersections".bstreetnam)'))
+    record = cursor.fetchall()
+    print(record)
