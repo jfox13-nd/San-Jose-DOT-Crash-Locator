bb10cf15ac59534edad2ff219acc564775c14133
Jack Fox <jfox13@nd.edu>
2020-02-24 15:44:38 -0800

    skeleton for getstreetfrominter function

---
+++ getstreetfrominter.sql
@@ -0,0 +1,39 @@
+CREATE OR REPLACE FUNCTION getstreetfrominter ( interId integer, streetaname VARCHAR(200), streetbname VARCHAR(200), astreetdir VARCHAR(20), bstreetdir VARCHAR(20), direction VARCHAR(200) )
+    RETURNS integer
+AS
+$body$
+DECLARE
+    streetID integer := 0;
+BEGIN
+
+    IF direction = 'East-West' THEN
+        IF astreetdir = 'East-West' THEN
+            --SELECT INTO streetID streetclean.id FROM streetclean, interclean where streetclean.street = streeta;
+            --streetID := (SELECT streetclean.id FROM streetclean, interclean where streetclean.street = streeta limit 1);
+            SELECT streetclean.id INTO streetID FROM streetclean, interclean where streetclean.street = streeta;
+        END IF;
+        IF bstreetdir = 'East-West' THEN
+            --SELECT INTO streetID streetclean.id FROM streetclean, interclean where streetclean.street = streetb;
+            --streetID := (SELECT streetclean.id FROM streetclean, interclean where streetclean.street = streetb limit 1);
+            SELECT streetclean.id INTO streetID FROM streetclean, interclean where streetclean.street = streetb;
+        END IF;
+    END IF;
+
+    IF direction = 'North-South' THEN
+        IF astreetdir = 'North-South' THEN
+            --SELECT INTO streetID streetclean.id FROM streetclean, interclean where streetclean.street = streeta;
+            --streetID := (SELECT streetclean.id FROM streetclean, interclean where streetclean.street = streeta limit 1);
+            SELECT streetclean.id INTO streetID FROM streetclean, interclean where streetclean.street = streeta;
+        END IF;
+        IF bstreetdir = 'North-South' THEN
+            --SELECT INTO streetID streetclean.id FROM streetclean, interclean where streetclean.street = streetb;
+            --streetID := (SELECT streetclean.id FROM streetclean, interclean where streetclean.street = streeta limit 1);
+            SELECT streetclean.id INTO streetID FROM streetclean, interclean where streetclean.street = streetb;
+        END IF;
+    END IF;
+
+    RETURN streetID;
+
+END;
+$body$
+LANGUAGE PLPGSQL;
