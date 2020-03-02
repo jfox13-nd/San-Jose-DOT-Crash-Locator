3543f8165d36aeca886feff68621c4fdc2aca8e5
Jack Fox <jfox13@nd.edu>
2020-03-02 10:06:43 -0800

    table name spelled incorrectly

--- findcrashlocation.sql
+++ findcrashlocation.sql
@@ -7,7 +7,7 @@ DECLARE
     streetID integer;
 BEGIN
 
-SELECT getstreetfrominter(interID, "Interclean".streeta, "Interclean".streetb, "Intersections".astreetdir, "Intersections".bstreetdir, direction) INTO streetID FROM "Intersections", interclean where "Intersections".id = interID and interclean.id = interID;
+SELECT getstreetfrominter(interID, interclean.streeta, interclean.streetb, "Intersections".astreetdir, "Intersections".bstreetdir, direction) INTO streetID FROM "Intersections", interclean where "Intersections".id = interID and interclean.id = interID;
 
 RETURN getnewlocation ( getpointonroad (interId, streetID), streetID, direction, distance);
 
