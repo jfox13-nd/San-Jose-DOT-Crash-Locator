7580b10584a106bc5b3da3e6e2289efc531e488f
Jack Fox <jfox13@nd.edu>
2020-03-02 10:31:08 -0800

    removed dead code

--- getstreetfrominter.sql
+++ getstreetfrominter.sql
@@ -8,26 +8,18 @@ BEGIN
 
     IF direction = 'East-West' OR direction = 'East' OR direction = 'West' THEN
         IF astreetdir = 'East-West' THEN
-            --SELECT INTO streetID streetclean.id FROM streetclean, interclean where streetclean.street = streeta;
-            --streetID := (SELECT streetclean.id FROM streetclean, interclean where streetclean.street = streeta limit 1);
             SELECT streetclean.id INTO streetID FROM streetclean, interclean where streetclean.street = streetaname limit 1;
         END IF;
         IF bstreetdir = 'East-West' THEN
-            --SELECT INTO streetID streetclean.id FROM streetclean, interclean where streetclean.street = streetb;
-            --streetID := (SELECT streetclean.id FROM streetclean, interclean where streetclean.street = streetb limit 1);
             SELECT streetclean.id INTO streetID FROM streetclean, interclean where streetclean.street = streetbname limit 1;
         END IF;
     END IF;
 
     IF direction = 'North-South' OR direction = 'North' OR direction = 'South' THEN
         IF astreetdir = 'North-South' THEN
-            --SELECT INTO streetID streetclean.id FROM streetclean, interclean where streetclean.street = streeta;
-            --streetID := (SELECT streetclean.id FROM streetclean, interclean where streetclean.street = streeta limit 1);
             SELECT streetclean.id INTO streetID FROM streetclean, interclean where streetclean.street = streetaname limit 1;
         END IF;
         IF bstreetdir = 'North-South' THEN
-            --SELECT INTO streetID streetclean.id FROM streetclean, interclean where streetclean.street = streetb;
-            --streetID := (SELECT streetclean.id FROM streetclean, interclean where streetclean.street = streeta limit 1);
             SELECT streetclean.id INTO streetID FROM streetclean, interclean where streetclean.street = streetbname limit 1;
         END IF;
     END IF;
