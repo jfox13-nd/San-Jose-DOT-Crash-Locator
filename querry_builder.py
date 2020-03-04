80aca67669c44079ab64891d64faaa30fef685b6
Jack Fox <jfox13@nd.edu>
2020-03-04 14:55:27 -0800

    removed dead code from querry_builder.py

--- querry_builder.py
+++ querry_builder.py
@@ -9,11 +9,8 @@ import psycopg2
 import sys
 
 STRINGCONVERSIONJSON = "conversions.json"
-DBNAME = "?????"
 DBLOCALNAME = "dot"
 USERNAME = "jfox13"
-EWINTERSTRING = "East-West"
-NSINTERSTRING = "North-South"
 
 def replace_string_builder(replace_json: str, col_name: str) -> str:
     ''' builds the piece of a querry responsible for doing all string replacements to clean data '''
@@ -21,7 +18,7 @@ def replace_string_builder(replace_json: str, col_name: str) -> str:
         replace_dict = json.load(f)
     replace_string = col_name
 
-    # TODO: resolve accurate way of handling a /
+    # TODO: resolve accurate way of handling a / in a street name (suggesting an alternative name for a street)
     # discards latter half of word if '/' is present
     replace_string = "REGEXP_REPLACE({}, \'/.*\', \'\')".format(replace_string)
 
@@ -34,59 +31,14 @@ def replace_string_builder(replace_json: str, col_name: str) -> str:
 
     return replace_string
 
-if __name__ == '__main__':
-    q = """
-select {} as a, {} as b from "StreetCenterlines", "Intersections"
-
-where {} not in (select {} from "StreetCenterlines") or
-{} not in (select {} from "StreetCenterlines");
-""".format( replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".astreetnam)'),
-            replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".bstreetnam)'),
-            replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".astreetnam)'),
-            replace_string_builder(STRINGCONVERSIONJSON,'LOWER("StreetCenterlines".fullname)'),
-            replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".bstreetnam)'),
-            replace_string_builder(STRINGCONVERSIONJSON,'LOWER("StreetCenterlines".fullname)'),
-)
-    q = """
-    INSERT INTO interclean (id, streeta, streetb)
-    SELECT id, {}, {} from "Intersections";
-
-    INSERT INTO streetclean (id, street)
-    SELECT id, {} from "StreetCenterlines";
-""".format( replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".astreetnam)'),
-            replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".bstreetnam)'),
-            replace_string_builder(STRINGCONVERSIONJSON,'LOWER("StreetCenterlines".fullname)'),
-)
-
-    q = """
-    INSERT INTO streetclean (id, street)
-    SELECT id, {} from "StreetCenterlines";
-""".format( replace_string_builder(STRINGCONVERSIONJSON,'LOWER("StreetCenterlines".fullname)') )
-
-    q = """
-    INSERT INTO interclean (id, streeta, streetb)
-    SELECT id, {}, {} from "Intersections";
-""".format( replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".astreetnam)'),
-            replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".bstreetnam)'),
-)
-
-
-
-    '''
-
+def connect_db():
+    ''' quickly connects to postgres database and returns cursor '''
     try:
         connection = psycopg2.connect(user = USERNAME,
                                     host = "127.0.0.1",
                                     port = "5432",
                                     database = DBLOCALNAME)
-        cursor = connection.cursor()
+        return connection.cursor()
     except:
         print("Error: Could not connect to SQL database {} as {}".format(DBLOCALNAME,USERNAME),file=sys.stderr)
         sys.exit()
-    #querry = 'select {}, {} from "Intersections" limit 2'.format(replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".astreetnam)'),replace_string_builder(STRINGCONVERSIONJSON,'LOWER("Intersections".bstreetnam)'))
-    querry = q
-    cursor.execute(querry)
-    record = cursor.fetchall()
-    print(record)
-    '''
-    print(q)
