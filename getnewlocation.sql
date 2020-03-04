63886858322009244817b1dfdccf0d6a68c213ae
Jack Fox <jfox13@nd.edu>
2020-03-04 10:35:21 -0800

    removed dead code

--- getnewlocation.sql
+++ getnewlocation.sql
@@ -5,43 +5,21 @@ $body$
 DECLARE
     fract_distance float8;
     inter_fract float8;
-    azimuth float8;
     final geometry;
-    line_start geometry;
 BEGIN
 
     SELECT distance / ST_Length(ST_AsText(ST_LineMerge(geom))) INTO fract_distance FROM "StreetCenterlines" WHERE id = streetID;
     SELECT ST_LineLocatePoint( ST_LineMerge(geom) , inter_point) INTO inter_fract FROM "StreetCenterlines" WHERE id = streetID;
-    SELECT ST_StartPoint( ST_LineMerge(geom) ) INTO line_start FROM "StreetCenterlines" WHERE id = streetID;
 
-    azimuth := ST_Azimuth(inter_point,line_start);
-
-    RAISE NOTICE 'fract_distance %', fract_distance;
-    RAISE NOTICE 'inter_fract %', inter_fract;
-
-    -- Solution to check if line starts from West or South not entirely correct if road turns in very different direction
-    /*
-    IF direction = 'West' THEN
-        --fract_distance := fract_distance * -1.0;
-        IF azimuth > PI() AND azimuth < 2.0 * PI() THEN
-            fract_distance := fract_distance * -1;
-        END IF;
-    END IF;
-    IF direction = 'South' THEN
-        --fract_distance := fract_distance * -1.0;
-        IF azimuth > PI() / 2.0 AND azimuth < 3.0 * PI() / 2.0 THEN
-            fract_distance := fract_distance * -1.0;
-        END IF;
-    END IF;
-    */
+    -- makes sure data is accurate wether the starting point is at the beginning or end of a line
     IF inter_fract > 0.5 THEN
         inter_fract := inter_fract - fract_distance;
     END IF;
-
     IF inter_fract < 0.5 THEN
         inter_fract := inter_fract + fract_distance;
     END IF;
 
+    -- if the fraction of the line is out of bounds then null is returned
     IF inter_fract < 0 OR inter_fract > 1 THEN
         RETURN NULL;
     END IF;
